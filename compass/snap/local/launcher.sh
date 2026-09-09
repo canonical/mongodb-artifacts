e15a0fd825c03923bf175729f52090a1978f94e5
Neha Oudin <neha@oudin.red>
2026-09-09 17:43:22 +0200

    fix: ask for system-observe permissions as well

--- compass/snap/local/launcher.sh
+++ compass/snap/local/launcher.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # Wrapper script for mongodb-compass applications to be run with restricted privileges
 
-connect_text="MongoDB Compass requires access to the password-manager-service interface
+connect_text="MongoDB Compass requires access to the password-manager-service and system-observe interfaces
 
-To connect the interface, in a terminal run:
+To connect the interfaces, in a terminal run:
 
-<tt><b>sudo snap connect mongodb-compass:password-manager-service</b></tt>"
-if ! snapctl is-connected password-manager-service; then
-  echo "password-manager-service not connected, please connect with:"
+<tt><b>sudo snap connect mongodb-compass:password-manager-service</b></tt>
+<tt><b>sudo snap connect mongodb-compass:system-observe</b></tt>"
+
+if ! snapctl is-connected password-manager-service || ! snapctl is-connected system-observe ; then
+  echo "password-manager-service and system-observe not connected, please connect with:"
   echo "sudo snap connect mongodb-compass:password-manager-service"
-  zenity --title "MongoDB Compass: Connect to Password Manager Service" --info --text "$connect_text" --width 600
+  echo "sudo snap connect mongodb-compass:system-observe"
+  zenity --title "MongoDB Compass: Connect to Password Manager Service and system-observe" --info --text "$connect_text" --width 600
   exit
 fi
 
