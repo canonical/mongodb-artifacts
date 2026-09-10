3c333abc5e57708d002b7e017ce9ec48978ddfa1
Neha Oudin <neha@oudin.red>
2026-09-10 10:26:17 +0200

    fix: smoke test

--- compass/tests/test_smoke.py
+++ compass/tests/test_smoke.py
@@ -27,8 +27,11 @@ def test_missing_password_manager_service():
             f"/snap/bin/{snapcraft['name']}", capture_output=True, text=True
         )
 
-        assert to_fail.returncode == 127
-        assert "password-manager-service not connected" in to_fail.stdout
+        assert to_fail.returncode == 1
+        assert (
+            "password-manager-service and system-observe not connected"
+            in to_fail.stdout
+        )
 
 
 @pytest.mark.run(after="test_missing_password_manager_service")
@@ -36,10 +39,16 @@ def test_with_password_manager_service():
     with open("snap/snapcraft.yaml") as file:
         snapcraft = yaml.safe_load(file)
 
-        connect_if = subprocess.run(
+        connect_password_manager = subprocess.run(
             f"sudo snap connect {snapcraft['name']}:password-manager-service".split(),
             capture_output=True,
             text=True,
         )
+        connect_system_observe = subprocess.run(
+            f"sudo snap connect {snapcraft['name']}:system-observe".split(),
+            capture_output=True,
+            text=True,
+        )
 
-        assert connect_if.returncode == 0
+        assert connect_password_manager.returncode == 0
+        assert connect_system_observe.returncode == 0
